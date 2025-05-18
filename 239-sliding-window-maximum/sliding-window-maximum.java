4afccc16c614a23c35d4d9ac40294d915327b7b0
divy6976 <divyprakashpandey6@gmail.com>
2025-05-18 09:02:03 +0530

    Time: 35 ms (32.75%) | Memory: 57.3 MB (97.18%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,36 +1,28 @@
 class Solution {
-     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k == 0) {
-            return new int[0];
-        }
-
-        List<Integer> result = new ArrayList<>();
-        Deque<Integer> dq = new ArrayDeque<>();
-
-        for (int i = 0; i < nums.length; i++) {
-           
-            if (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayList<Integer> list= new ArrayList<>();
+Deque<Integer> dq=new ArrayDeque<>();
+        for(int i=0;i<nums.length;i++){
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k){
                 dq.removeFirst();
             }
+        
+        while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]){
+            dq.removeLast();
+        }
+       dq.addLast(i);
 
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
-                dq.removeLast();
-            }
-
-            dq.addLast(i);
-
-    
-            if (i >= k - 1) {
-                result.add(nums[dq.peekFirst()]);
-            }
+        if(i>=k-1){
+            list.add(nums[dq.peekFirst()]);
         }
 
 
-        int[] ans = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            ans[i] = result.get(i);
-        }
 
+        }
+        int ans[]=new int[list.size()];
+        for(int i=0;i<ans.length;i++){
+            ans[i]=list.get(i);
+        }
         return ans;
     }
 }
