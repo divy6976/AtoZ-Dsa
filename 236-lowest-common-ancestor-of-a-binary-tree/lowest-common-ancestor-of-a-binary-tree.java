bf3bf26893bea8aade80617dd4fa0fb62c67b2e7
divy6976 <divyprakashpandey6@gmail.com>
2025-05-23 11:04:34 +0530

    Time: 7 ms (64.64%) | Memory: 45.3 MB (14.84%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,50 +8,19 @@
  * }
  */
 class Solution {
-boolean forp(TreeNode  node,TreeNode p,ArrayList<TreeNode>list1){
-    if(node==null) return false;
-
-    list1.add(node);
-    if(node.val==p.val) return true;
-    if(forp(node.left,p,list1) || forp(node.right,p,list1)){
-        return true;
-    }
-    list1.remove(list1.size()-1);
-    return false;
-}
-
-boolean forq(TreeNode  node,TreeNode q,ArrayList<TreeNode>list2){
-    if(node==null) return false;
-
-    list2.add(node);
-    if(node.val==q.val) return true;
-    if(forq(node.left,q,list2) || forq(node.right,q,list2)){
-        return true;
-    }
-    list2.remove(list2.size()-1);
-    return false;
-}
-
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-        ArrayList<TreeNode> list1=new ArrayList<>();
-        ArrayList<TreeNode> list2=new ArrayList<>();
-        TreeNode result=new TreeNode(0);
-        forp(root,p,list1);
-        forq(root,q,list2);
-        for(int i=0;i<list1.size();i++){
-            for(int j=i;j<list2.size();j++){
-             
-               if(list1.get(i)==list2.get(j)){
-                result=list2.get(j);
-                break;
-               }
-            }
-        
+        if(root==null ||root==p||root==q){
+            return root;
         }
-
-return result;
+     TreeNode left=   lowestCommonAncestor(root.left,p,q);
+   TreeNode right=   lowestCommonAncestor(root.right,p,q);
+   if(left==null){
+    return right;
+   }else if(right==null){
+    return left;
+   }else{
+    return root;
+   }
 
     }
 }
