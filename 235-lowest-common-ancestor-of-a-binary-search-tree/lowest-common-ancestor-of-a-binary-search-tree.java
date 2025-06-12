1ffa3c5bc5e59de87801ea718c1087a32ab544ef
divy6976 <divyprakashpandey6@gmail.com>
2025-06-12 19:55:47 +0530

    Time: 6 ms (29.61%) | Memory: 45.5 MB (10.30%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,23 +9,22 @@
  */
 
 class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
 
-   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    // Base case: if root is null or root is one of the nodes p or q
-    if (root == null || root == p || root == q) {
-        return root;
+        TreeNode left = null, right = null;
+
+        if (p.val < root.val && q.val < root.val) {
+            left = lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            right = lowestCommonAncestor(root.right, p, q);
+        } else {
+            // This means p and q lie on either side of root
+            return root;
+        }
+
+        return left != null ? left : right;
     }
-    
-    // Recursively search in the left and right subtrees
-    TreeNode left = lowestCommonAncestor(root.left, p, q);
-    TreeNode right = lowestCommonAncestor(root.right, p, q);
-if(left==null){
-    return right;
-}else if(right==null){
-return left;
-}else{
-    return root;
-}
-
-}
 }
