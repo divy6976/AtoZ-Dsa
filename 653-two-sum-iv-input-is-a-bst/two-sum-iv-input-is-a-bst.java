c07dd9ad0e6c1353684a6d913b703f8044bb8c8f
divy6976 <divyprakashpandey6@gmail.com>
2025-06-13 21:37:41 +0530

    Time: 3 ms (65.07%) | Memory: 45.2 MB (50.76%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -13,25 +13,56 @@
  *     }
  * }
  */
-class Solution {
-    ArrayList<Integer> list=new ArrayList<>();
-    void inorder(TreeNode node){
-        if(node==null) return;
-        inorder(node.left);
-        list.add(node.val);
-        inorder(node.right);
-
-    }
-    public boolean findTarget(TreeNode root, int k) {
-        inorder(root);
-        for(int i=0;i<list.size();i++){
-            for(int j=i+1;j<list.size();j++){
-                if((list.get(i)+list.get(j))==k){
-                    return true;
-                }
-            }
-            
+   class BSTIterator{
+      private  Stack<TreeNode>st=new Stack<>();
+        boolean reverse=true;
+        public BSTIterator(TreeNode node, boolean isReverse){
+reverse=isReverse;
+pushAll(node);
         }
-        return false;
+       boolean hasNext(){
+        return !st.isEmpty();
+       }
+       int next(){
+        TreeNode temp=st.pop();
+        if(reverse==false){
+            pushAll(temp.right);
+        }else{
+            pushAll(temp.left);
+        }
+return temp.val;
+       }
+  private  void   pushAll(TreeNode node){
+        while(node!=null){
+            st.push(node);
+if (reverse == false) {
+                node = node.left;  // ✅ FIXED: go left for in-order
+            } else {
+                node = node.right; // ✅ FIXED: go right for reverse in-order
+            }
+        }
+       }
+    }
+class Solution {
+  
+    public boolean findTarget(TreeNode root, int k) {
+       if(root==null) return false;
+       BSTIterator l=new BSTIterator(root,false);
+       BSTIterator r=new BSTIterator(root,true);
+       int i=l.next();
+       int j=r.next();
+
+  while (i < j) {
+            if (i + j == k) {
+                return true;
+            } else if (i + j < k) {
+                if (l.hasNext()) i = l.next(); // ✅ Fix: Safe check before calling next()
+                else break;
+            } else {
+                if (r.hasNext()) j = r.next(); // ✅ Fix: Safe check before calling next()
+                else break;
+            }
+        }
+return false;
     }
 }
