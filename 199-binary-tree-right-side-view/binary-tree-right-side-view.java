3631dbc4fc9f47d4f4ce6251192c32e033aa3af4
divy6976 <divyprakashpandey6@gmail.com>
2025-06-17 22:14:48 +0530

    Time: 1 ms (71.48%) | Memory: 42.2 MB (43.06%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,31 +15,27 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        Queue<TreeNode>queue=new LinkedList<>();
-        if(root==null){
-            return ans;
-        }
+
+        ArrayList<Integer> result=new ArrayList<>();
+        if(root==null) return result;
+        Queue<TreeNode> queue=new LinkedList<>();
         queue.offer(root);
         while(!queue.isEmpty()){
-            int level=queue.size();
-            ArrayList<Integer> numlist=new ArrayList<>();
-            for(int i=0;i<level;i++){
-                TreeNode node=queue.poll();
-                if(node.left!=null){
-                    queue.offer(node.left);
+           int level=queue.size();
+           for(int i=0;i<level;i++){
+             TreeNode node=queue.poll();
+             if(i==level-1){
+                result.add(node.val);
+             }
+             if(node.left!=null){
+                queue.offer(node.left);
+             }
+             if(node.right!=null){
+                queue.offer(node.right);
+             }
+           }
 
-
-                }
-                if(node.right!=null){
-                    queue.offer(node.right);
-                }
-
-numlist.add(node.val);
-
-            }
-            ans.add(numlist.get(numlist.size()-1));
         }
-return ans;
+return result;
     }
 }
