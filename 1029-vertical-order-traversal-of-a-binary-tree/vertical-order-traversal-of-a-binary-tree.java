c5ca7524ec05d25844f8a477d1eee7561707b4a9
divy6976 <divyprakashpandey6@gmail.com>
2025-06-18 21:54:25 +0530

    Time: 3 ms (81.55%) | Memory: 41.9 MB (99.66%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -14,44 +14,36 @@
  * }
  */
 class Solution {
-     TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> map=new TreeMap<>();
-
-void dfs(TreeNode node,int col,int level){
-    if(node==null){
-        return ;
-    }
-    if(!map.containsKey(col)){
-        map.put(col,new TreeMap<>());
-    }
-    if(!map.get(col).containsKey(level)){
-     map.get(col).put(level,new ArrayList<>());
-    }
-map.get(col).get(level).add(node.val);
-dfs(node.left,col-1,level+1);
-dfs(node.right,col+1,level+1);
-}
-
-
-
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-    List<List<Integer>> res=new ArrayList<>();
-    if(root==null){
-        return  res;
-
-    }
-  
-    dfs(root,0,0);
-    for(Map.Entry<Integer,TreeMap<Integer,ArrayList<Integer>>> entry:map.entrySet()){
-        TreeMap<Integer,ArrayList<Integer>> levelmap=entry.getValue();
-        ArrayList<Integer>list=new ArrayList<>();
-        for(Map.Entry<Integer,ArrayList<Integer>> subentry:levelmap.entrySet()){
-            ArrayList<Integer> sublist=subentry.getValue();
-            Collections.sort(sublist);
-            list.addAll(sublist);
-
+    TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> map=new TreeMap<>();
+    void dfs(TreeNode node,int col,int level){
+        if(node== null ) return ;
+        if(!map.containsKey(col)){
+            map.put(col,new TreeMap<>());
         }
-        res.add(list);
+        if(!map.get(col).containsKey(level)){
+            map.get(col).put(level,new ArrayList<>());
+        }
+        map.get(col).get(level).add(node.val);
+        dfs(node.left,col-1,level+1);
+        dfs(node.right,col+1,level+1);
     }
-return res;
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        dfs(root,0,0);
+        for(Map.Entry<Integer,TreeMap<Integer,ArrayList<Integer>>> entry:map.entrySet()){
+            TreeMap<Integer,ArrayList<Integer>> levelmap=entry.getValue();
+            ArrayList<Integer> list=new ArrayList<>();
+            for(Map.Entry<Integer,ArrayList<Integer>> subentry:levelmap.entrySet()){
+                ArrayList<Integer> sublist=subentry.getValue();
+                Collections.sort(sublist);
+                list.addAll(sublist);
+
+            }
+            result.add(list);
+        }
+        return result;
     }
 }
