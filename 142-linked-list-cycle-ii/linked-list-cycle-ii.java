b7438d2eb05b42769f1a24a40387a32502bfe6c8
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-03-14 09:30:27 +0530

    Time: 7 ms (9.81%) | Memory: 46.4 MB (96.19%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -12,49 +12,60 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
 
-        boolean check=false;
+        // boolean check=false;
 
-        if(head == null || head.next == null){
-            return null;
-        }
+        // if(head == null || head.next == null){
+        //     return null;
+        // }
 
-        ListNode fast=head;
-        ListNode slow=head;
-        ListNode prev=null;
+        // ListNode fast=head;
+        // ListNode slow=head;
+        // ListNode prev=null;
 
-        while(fast!=null && fast.next!=null && fast.next.next!=null){
+        // while(fast!=null && fast.next!=null && fast.next.next!=null){
 
-            prev=fast;
-            fast=fast.next.next;
-            slow=slow.next;
+        //     prev=fast;
+        //     fast=fast.next.next;
+        //     slow=slow.next;
 
-            if(fast == slow){
-                check = true;
-                break;
+        //     if(fast == slow){
+        //         check = true;
+        //         break;
                
                
 
-            }
+        //     }
 
-        }
-        ListNode temp= head;
+        // }
+        // ListNode temp= head;
 
 
-        if(check){
-        while(temp!=null){
+        // if(check){
+        // while(temp!=null){
             
-            if(temp == slow ){
-                return temp;
-            }
-            temp=temp.next;
-            slow=slow.next;
-        }
-        }
+        //     if(temp == slow ){
+        //         return temp;
+        //     }
+        //     temp=temp.next;
+        //     slow=slow.next;
+        // }
+        // }
 
 
 
        
+        // return null;
+        HashSet<ListNode > st=new HashSet<>();
+
+        ListNode temp=head;
+        while(temp!=null){
+            if(!st.contains(temp)){
+                st.add(temp);
+            }else {
+                return temp;
+            }
+            temp=temp.next;
+        }
         return null;
-        
     }
 }
