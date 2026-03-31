f1686e9aa8f5c01697eb3a44e1e35e5173e416ae
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-03-31 20:34:21 +0530

    Time: 6 ms (74.21%) | Memory: 47 MB (71.24%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,42 +1,47 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj=new ArrayList<>(); 
-        Queue<Integer> q=new LinkedList<>();
-        int n=numCourses;
-        int indegree[]=new int[n];
-        for(int i=0;i<n;i++){
-   adj.add(new ArrayList<>());
+
+        int[] indegree=new int[numCourses];
+
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
         }
-for(int[]edge:  prerequisites){
-    int u=edge[1];
-    int v=edge[0];
-    adj.get(u).add(v);
-}
-for(int i=0;i<n;i++){
-    for(int it:adj.get(i)){
-indegree[it]++;
-    }
-}
-for(int i=0;i<n;i++ ){
+
+       for(int [] edge:prerequisites){
+        int u=edge[0];
+        int v=edge[1];
+        indegree[u]++;
+        adj.get(v).add(u);
+
+       }
+
+    Queue<Integer> q=new LinkedList<>();
+
+   for(int i=0;i<indegree.length;i++){
     if(indegree[i]==0){
         q.offer(i);
     }
-}
-while(!q.isEmpty()){
-    int node=q.poll();
-    for(int it:adj.get(node)){
-        indegree[it]--;
-        if(indegree[it]==0){
-            q.offer(it);
-        }
-    }
-}
-for(int i=0;i<n;i++){
-    if(indegree[i]!=0){
-        return false;
-    }
-}
-return true;
-    }
+   }
 
+     while(!q.isEmpty()){
+        int num=q.poll();
+
+        for(int neighbour:adj.get(num)){
+            indegree[neighbour]--;
+            if(indegree[neighbour]== 0){
+                q.offer(neighbour);
+            }
+        }
+     }
+
+     for(int i=0;i<indegree.length;i++){
+        if(indegree[i] > 0){
+            return false;
+        }
+     }
+    return true;
+        
+    }
 }
