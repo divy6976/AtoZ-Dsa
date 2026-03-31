ab695f2ded2ca9900d44ea5d5c241f020f84caa2
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-03-31 20:55:34 +0530

    Time: 6 ms (49.46%) | Memory: 46.8 MB (63.13%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,50 +1,73 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int []result=new int[numCourses];
-        int n=numCourses;
-int indegree[]=new int[n];
-List<List<Integer>> adj=new ArrayList<>();
-List<Integer> ans=new ArrayList<>();
-Queue<Integer> q=new LinkedList<>();
-for(int i=0;i<n;i++){
-    adj.add(new ArrayList<>());
 
-}
-for(int[]edge: prerequisites){
-    int u=edge[1];
-    int v=edge[0];
-    adj.get(u).add(v);
+        ArrayList<Integer> ans = new ArrayList<>();
 
-}
-for(int i=0;i<n;i++){
-    for(int it:adj.get(i)){
-        indegree[it]++;
-    }
-}
-for(int i=0;i<n;i++){
-    if(indegree[i]==0){
-        q.offer(i);
-    }
-}
-while(!q.isEmpty()){
-    int node=q.poll();
- ans.add(node);
- for(int it:adj.get(node)){
-    indegree[it]--;
-    if(indegree[it]==0){
-        q.offer(it);
-    }
- }
-}
-if (ans.size() != numCourses) {
-    return new int[0]; // cycle exists
-}
+        if(prerequisites.length == 0 ){
+            
+            for(int i=0;i<numCourses;i++){
+                ans.add(i);
+            }
+            Collections.reverse(ans);
+            int[] arr=new int[ans.size()];
+            for(int i=0;i<arr.length;i++){
+                arr[i]=ans.get(i);
+            }
 
-for (int i = 0; i < numCourses; i++) {
-    result[i] = ans.get(i);
-}
+            return arr;
+        }
 
-      
-return result;
+        ArrayList<ArrayList<Integer> > adj=new ArrayList<>();
+        int[] indegree=new int[numCourses];
+
+        for(int i=0;i<indegree.length;i++){
+            adj.add(new ArrayList());
+        }
+
+        for(int [] edge:prerequisites){
+            int u=edge[0];
+            int v=edge[1];
+            indegree[u] ++;
+            adj.get(v).add(u);
+
+        }
+
+        Queue<Integer> q=new LinkedList<>();
+        
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+
+        }
+        
+        while(!q.isEmpty()){
+            int num=q.poll();
+            ans.add(num);
+            for(int neighbour:adj.get(num)){
+                indegree[neighbour]--;
+                if(indegree[neighbour] ==0){
+                    q.offer(neighbour);
+                }
+            }
+        }
+
+
+
+        int[] arr=new int[ans.size()];
+
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i] >0){
+              return new int[0];
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]=ans.get(i);
+        }
+        return arr;
+
+
+        
     }
 }
