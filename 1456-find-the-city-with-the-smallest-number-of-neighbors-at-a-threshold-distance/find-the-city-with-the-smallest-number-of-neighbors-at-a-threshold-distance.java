d411e49d90208ed7be37c88bb40ba60bee7e5dde
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-04-09 09:53:04 +0530

    Time: 7 ms (99.65%) | Memory: 46.3 MB (81.28%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,94 +1,59 @@
 class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+   int[][] matrix = new int[n][n];
 
-    class Pair{
-        int num;
-        int dist;
-        Pair(int dist,int num){
-           
-            this.dist=dist;
-             this.num=num;
-        }
-    }
-
-    void floyd(int row,int[] freq,ArrayList<ArrayList<Pair>> adj,int distance){
-        PriorityQueue<Pair> pq=new PriorityQueue<>((Pair x,Pair y) ->{
-            return x.dist-y.dist;
-        });
-        int[] visited=new int[freq.length];
-        for(int i=0;i<visited.length;i++){
-            visited[i]=Integer.MAX_VALUE;
-        }
-        int cnt=0;
-        boolean[] check=new boolean[freq.length];
-
-        pq.offer(new Pair(0,row));
-        visited[row]=0;
-        check[row]=true;
-
-        while(!pq.isEmpty()){
-            Pair p=pq.poll();
-            int dist=p.dist;
-
-            int num=p.num;
-            
-            if(!check[num]){
-                cnt ++;
-                check[num]=true;
+       
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j) matrix[i][j]=0;
+                else matrix[i][j]=100000000; // infinity
             }
-            for(Pair d:adj.get(num)){
-                int weight=d.dist;
-                int node=d.num;
-                int newdist=weight+dist;
-                
-                if(newdist <= distance && newdist < visited[node]){
-                  
-                    pq.offer(new Pair(newdist,node));
-                    visited[node]=newdist;
+        }
+
+       
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            int w=edge[2];
+            matrix[u][v]=w;
+            matrix[v][u]=w;
+        }
+
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(matrix[i][k] + matrix[k][j] < matrix[i][j]){
+                        matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    }
                 }
             }
         }
-        freq[row]=cnt-1;
+        int[] freq = new int[n];
 
-    }
+for(int i = 0; i < n; i++){
+    int count = 0;
 
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-
-        
-    ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
-    int[] freq=new int[n];
-    for(int i=0;i<n;i++){
-        adj.add(new ArrayList<>());
-    }
-
-    for(int[] edge:edges){
-        int u=edge[0];
-        int v=edge[1];
-        int w=edge[2];
-
-        adj.get(u).add(new Pair(w,v));
-        adj.get(v).add(new Pair(w,u));
-
-    }
-    int distance=distanceThreshold;
-
-    for(int i=0;i<n;i++){
-        floyd(i,freq,adj,distanceThreshold);
-    }
-
-    int mini=Integer.MAX_VALUE;
-    int ans=0;
-    for(int i=0;i<freq.length;i++){
-        if(freq[i] <= mini){
-            mini=freq[i];
-            ans=Math.max(ans,i);
+  
+    for(int j = 0; j < n; j++){
+        if(i != j && matrix[i][j] <= distanceThreshold){
+            count++;
         }
     }
 
-  return ans;
+   
+    freq[i] = count;
+}
 
+int min = Integer.MAX_VALUE;
+int ans = -1;
 
-
-       
+for(int i = 0; i < n; i++){
+    if(freq[i] <= min){
+        min = freq[i];
+        ans = i; // tie me automatically larger index le lega
+    }
+}
+return ans;
 
         
     }
