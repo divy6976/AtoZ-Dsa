097ee7498676f4b32060ed8f706c1e4ce2293855
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-04-11 11:06:21 +0530

    Time: 13 ms (34.88%) | Memory: 69.4 MB (25.65%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,57 +1,69 @@
 class Solution {
+       ArrayList<Integer> size=new ArrayList<>();
+        ArrayList<Integer>  parent=new ArrayList<>();
+    class DisjointSet{
+     
 
-   void bfs(int i,boolean[] visited,ArrayList<ArrayList<Integer>> adj){
-        Queue<Integer> q=new LinkedList<>();
-        q.offer(i);
-        visited[i]=true;
-
-        while(!q.isEmpty()){
-            int num=q.poll();
-            for(int neighbour:adj.get(num)){
-                if(!visited[neighbour]){
-                    visited[neighbour]= true;
-                    q.offer(neighbour);
-                }
+     DisjointSet(int n){
+            for(int i=0;i<n;i++){
+                parent.add(i);
+                size.add(1);
             }
         }
 
+        int findparent(int node){
+            if(parent.get(node) == node){
+                return node;
+            }
+            int ulp=findparent(parent.get(node));
+            parent.set(node,ulp);
+            return ulp;
+        }
 
+        void unionbysize(int u,int v){
+            int ulpu=findparent(u);
+            int ulpv=findparent(v);
 
+            if(ulpu == ulpv ) return ;
+            if(size.get(ulpu) < size.get(ulpv)){
+                parent.set(ulpu,ulpv);
+                size.set(ulpv,size.get(ulpu)+size.get(ulpv));
+            }else {
+                parent.set(ulpv,ulpu);
+                size.set(ulpu,size.get(ulpu)+size.get(ulpv));
+            }
+        }
 
     }
     public int makeConnected(int n, int[][] connections) {
 
-        int edge=0;
+        DisjointSet ds = new DisjointSet(n);
+        int edges=0;
 
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
 
         for(int[] conn:connections){
+            edges++;
             int u=conn[0];
             int v=conn[1];
-            edge++;
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-
+            ds.unionbysize(u,v);
 
         }
         int cnt =0;
-        boolean[] visited=new boolean[n];
 
-        for(int i=0;i<n;i++){
-            
-            if(visited[i] == false){
+        for(int i=0;i<parent.size();i++){
+            if(i==parent.get(i) ){
                 cnt ++;
-                bfs(i,visited,adj);
             }
-        } 
-        if(edge <n-1){
-            return -1;
+        }
+
+        if(edges  < n-1){
+             return -1;
         }
         return cnt-1;
+
+       
+
+
 
         
     }
