176daaa5e080f5e3cba02f343518cb7560aa00f9
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-04-16 14:24:07 +0530

    Time: 499 ms (12.13%) | Memory: 60.5 MB (8.26%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,43 @@
 class Solution {
+    // x= -(y+z)
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        HashSet<ArrayList<Integer>> st=new HashSet<>();
+
         for(int i=0;i<nums.length-2;i++){
-          
-            if(i>0 &&nums[i]==nums[i-1]) continue;
 
-            int j=i+1;
-            int k=nums.length-1;
-            while(j<k){
-                if(nums[i]+nums[j]+nums[k]>0){
-                    k--;
-                }else if(nums[i]+nums[j]+nums[k]<0){
-                    j++;
-                }else {
-                      ArrayList<Integer> sublist=new ArrayList<>();
+            int y=i+1;
+            int z=nums.length-1;
+
+            while(y<z){
+                int target=-(nums[i]);
+                int check= (nums[y]+nums[z]);
+
+                if(check == target){
+                    ArrayList<Integer> sublist=new ArrayList<>();
                     sublist.add(nums[i]);
-                    sublist.add(nums[j]);
-                    sublist.add(nums[k]);
-                    result.add(sublist);
-                    j++;
-                    k--;
-                                        // ✅ Skip duplicates
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
-
+                    sublist.add(nums[y]);
+                    sublist.add(nums[z]);
+                    Collections.sort(sublist);
+                    st.add(sublist);
+                    y++;
+                    z--;
+                    
+                    
+                }else if(check  > target){
+                    z--;
+                }else {
+                    y++;
                 }
             }
+                     
 
+        }
 
-
-
-
-                    }
-return result;
+         for(ArrayList sublist:st){
+                ans.add(sublist);
+            }
+            return ans;
     }
 }
