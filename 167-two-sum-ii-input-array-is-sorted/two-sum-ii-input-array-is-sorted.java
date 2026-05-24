36921bd692b56931de6bbe6f5fbaf459fc7be872
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-05-24 09:40:40 +0530

    Time: 9 ms (7.78%) | Memory: 49.1 MB (5.00%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -6,15 +6,22 @@ class Solution {
         HashMap<Integer,Integer> mpp=new HashMap<>();
 
         for(int i=0;i<numbers.length;i++){
+
             int check=target-numbers[i];
+
             if(mpp.containsKey(check)){
                 ans[0]=mpp.get(check)+1;
                 ans[1]=i+1;
             }
+
             mpp.put(numbers[i],i);
+
+
         }
 
         return ans;
+
+
         
     }
 }
