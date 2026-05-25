1bda06115c6c745604c9e5dd515f8df146c86af2
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-05-25 21:23:21 +0530

    Time: 3 ms (88.01%) | Memory: 43.4 MB (40.19%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,41 +3,54 @@ class Solution {
 
         char ch=s.charAt(0);
 
-        if(ch== ')' || ch == '}' || ch==']'){
+        if(ch == '}' || ch==']' || ch==')'){
             return false;
         }
-        Stack<Character> st=new Stack<>();
+
+        Stack<Character > st=new Stack<>();
 
         for(int i=0;i<s.length();i++){
             char dh=s.charAt(i);
-            if(st.isEmpty() ){
-                if(dh== ')' || dh=='}' || dh==']'){
+
+            if(dh == '[' || dh=='{' || dh=='('){
+                st.push(dh);
+            }else if(dh == ']'){
+                if(!st.isEmpty()){
+                char po=st.pop();
+                if(po == '{' || po == '('){
+                    return false;
+
+                }
+                }else {
+                    return false;
+                }
+            }else if(dh == '}'){
+                if(!st.isEmpty()){
+                char po=st.pop();
+
+                if(po == '[' || po =='('){
+                    return false;
+                }
+                }else{
+                    return false;
+                }
+            }else if(dh ==')'){
+                if(!st.isEmpty()){
+                char po=st.pop();
+                if(po == '{' || po == '['){
+                    return false;
+                }
+                }else{
                     return false;
                 }
             }
 
-            
-            if(dh == '(' || dh=='{' || dh== '['){
-                st.push(dh);
-            }else if(dh== ')' || dh=='}' || dh==']'){
-                if(!st.isEmpty()){
-                    if((st.peek() =='(' && dh==')') || (st.peek()=='{' && dh=='}') || (st.peek()=='[' && dh==']')){
-                        st.pop();
-                    }else {
-                        return false;
-                    }
-                }
-            }
-
-
-
         }
-        if(st.isEmpty()){
-            return true;
+
+        if(!st.isEmpty()){
+            return false;
         }
-        return false;
-
-
+        return true;
         
     }
 }
