038af0ac2cfcc81e3860c21cfd321d31cea43689
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-06 12:24:13 +0530

    Time: 2 ms (94.94%) | Memory: 45.8 MB (49.66%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,25 +1,49 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>>  result=new ArrayList<>();
-        ArrayList<Integer> current=new ArrayList<>();
-        Arrays.sort(candidates);
-        combination(candidates,target,0,result,current);
-        return result;
+
+
+    void check(int ind,List<List<Integer>> ans,ArrayList<Integer> sublist,int[] candidates,int target,int sum){
+
+        if(ind >= candidates.length){
+            if(sum == target){
+                ans.add(new ArrayList<>(sublist));
+            }
+            return ;
+        }
+
+        if(sum == target){
+            ans.add(new ArrayList<>(sublist));
+            return ;
+        }
+
+        if(sum > target){
+            return ;
+        }
+
+        sum += candidates[ind];
+        sublist.add(candidates[ind]);
+        check(ind,ans,sublist,candidates,target,sum);
+        sublist.remove(sublist.size()-1);
+        sum -=candidates[ind];
+
+        check(ind+1,ans,sublist,candidates,target,sum);
+
+
     }
-  void  combination(int[] candidates,int target,int ind,   List<List<Integer>>  result,     ArrayList<Integer> current){
+
+
+
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+
+        List<List<Integer>> ans=new ArrayList<>();
+
+        ArrayList<Integer> sublist=new ArrayList<>();
+
+
+        check(0,ans,sublist,candidates,target,0);
+
+        return ans;
         
-if(target==0){
-    result.add(new ArrayList<>(current));
-    return;
-}
-for(int i=ind;i<candidates.length;i++){
-    if(candidates[i]>target) break;
-    current.add(candidates[i]);
-    combination(candidates,target-candidates[i],i,result,current);
-    current.remove(current.size()-1);
-}
-
-
-
     }
 }
