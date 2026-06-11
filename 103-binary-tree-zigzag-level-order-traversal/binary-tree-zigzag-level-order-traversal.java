419467b84f8428b396ee95e2c21597294a37da41
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-11 10:25:56 +0530

    Time: 1 ms (78.48%) | Memory: 43.9 MB (24.06%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,43 +14,56 @@
  * }
  */
 class Solution {
-  
-
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-     List<List<Integer>> result=new ArrayList<>();
-    
-     if(root==null){
-        return result;
-     }
-boolean direc = true;
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int level=queue.size();
-             ArrayList<Integer> list=new ArrayList<>();
-            for(int i=0;i<level;i++){
-                
-                TreeNode node=queue.poll();
 
-                    if(node.left!=null){
-                        queue.offer(node.left);
-                    }
-                    if(node.right!=null){
-                        queue.offer(node.right);
-                    }
 
-               if(direc== true){
-                list.add(node.val);
-               }else{
-                list.add(0,node.val);
-               }
-              
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+        
+
+        
+        Queue<TreeNode >q=new LinkedList<>();
+        q.offer(root);
+  boolean flag =true;
+
+        while(!q.isEmpty()){
+          
+            int check=q.size();
+            ArrayList<Integer> sublist=new ArrayList<>();
+            for(int i=0;i<check;i++){
+                TreeNode node=q.poll();
+
+                if(node.left !=null){
+                    q.offer(node.left);
+                }
+                if(node.right !=null){
+                    q.offer(node.right);
+                }
+
+                sublist.add(node.val);
+
             }
-         direc=!direc;
-          result.add(new ArrayList<>(list));
-
+            if(flag){
+                ans.add(new ArrayList<>(sublist));
+            }else {
+               Collections.reverse(sublist);
+               ans.add(new ArrayList<>(sublist));
+            }
+            if(flag){
+                flag=false;
+            }else {
+                flag = true;
+            }
         }
 
-        return result;
+
+        return ans;
+
+
+
+
+        
     }
 }
