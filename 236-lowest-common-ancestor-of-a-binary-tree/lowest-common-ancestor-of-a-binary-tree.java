bfa7bc46da03a527da4e3daf920ee87acf4cc59e
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-12 19:39:40 +0530

    Time: 40 ms (5.02%) | Memory: 80.3 MB (5.04%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,19 +8,65 @@
  * }
  */
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null ||root==p||root==q){
-            return root;
+
+    
+
+    boolean check(TreeNode node,TreeNode target,ArrayList<TreeNode> ans){
+
+        if(node == null ) {
+            return  false;
         }
-     TreeNode left=   lowestCommonAncestor(root.left,p,q);
-   TreeNode right=   lowestCommonAncestor(root.right,p,q);
-   if(left==null){
-    return right;
-   }else if(right==null){
-    return left;
-   }else{
-    return root;
+
+        if(node == target){
+            
+            ans.add(node);
+            return true;
+        }
+
+        ans.add(node);
+        if(check(node.left,target,ans)||check(node.right,target,ans)){
+            return true;
+        }
+   if(ans.size()!=0){
+      ans.remove(ans.size()-1);
    }
 
+
+
+    return false;
+
+
+
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+
+        ArrayList<TreeNode> pee=new ArrayList<>();
+        ArrayList<TreeNode> qq=new ArrayList<>();
+        
+       
+        check(root,p,pee);
+
+      
+        check(root,q,qq);
+
+
+
+
+        TreeNode temp=null;
+        for(int i=0;i<Math.min(pee.size(),qq.size()) ; i++){
+
+            if(pee.get(i) != qq.get(i)){
+                break;
+            }
+            temp=pee.get(i);
+        }
+
+        return temp;
+
+
+
+        
     }
 }
