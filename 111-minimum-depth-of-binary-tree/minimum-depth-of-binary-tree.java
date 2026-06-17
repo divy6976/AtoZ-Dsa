1867d1ac2d33226b42a04e8a1eca717feddeb7c1
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-17 12:28:59 +0530

    Time: 12 ms (7.28%) | Memory: 85.2 MB (11.86%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -14,42 +14,56 @@
  * }
  */
 class Solution {
+
+    class Pair{
+        TreeNode node;
+        int cnt;
+        Pair(TreeNode node,int cnt){
+            this.node=node;
+            this.cnt=cnt;
+        }
+    }
+
+
+  
+
     public int minDepth(TreeNode root) {
 
         if(root == null){
             return 0;
         }
 
-        if(root.left == null && root.right == null){
-            return 1;
+        int mini=Integer.MAX_VALUE;
+
+        Queue<Pair> q=new LinkedList<>();
+
+        q.offer(new Pair(root,1));
+
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+              Pair p=q.poll();
+              TreeNode node =p.node;
+              int cnt=p.cnt;
+
+               if(node.left == null && node.right == null){
+
+                mini=Math.min(mini,cnt);
+                continue;
+              }
+
+              if(node.left!=null){
+                q.offer(new Pair(node.left,cnt+1));
+              }
+              if(node.right!=null){
+                q.offer(new Pair(node.right,cnt+1));
+              }
+             
+
+            }
         }
+  return mini;
 
-        int cnt=Integer.MAX_VALUE;
-        int compare =1;
-     
-     Queue<TreeNode > q=new LinkedList<>();
 
-     q.offer(root);
-
-     while(!q.isEmpty()){
-        int check=q.size();
-        for(int i=0;i<check;i++){
-            TreeNode node =q.poll();
-            if(node.left != null){
-                q.offer(node.left);
-            }
-            if(node.right !=null){
-                q.offer(node.right);
-            }
-            if(node.left == null && node.right == null){
-                cnt = Math.min(cnt,compare);
-            }
-        }
-        compare ++;
-     }
-
-     return cnt;
-
-        
     }
 }
