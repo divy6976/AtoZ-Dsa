37822fb056742b7879950600db3d5b7b0fdd80ad
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-27 07:08:35 +0530

    Time: 110 ms (23.36%) | Memory: 71.4 MB (11.48%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,26 +1,33 @@
 class Solution {
     public int countPrimes(int n) {
 
+        if(n==0 || n==1 || n==2){
+            return 0;
+
+        }
+
         int[] prime=new int[n];
         Arrays.fill(prime,1);
+        prime[0]=0;
+        prime[1]=0;
 
         for(int i=2;i*i<n;i++){
             if(prime[i] == 1){
+  
                 for(int j=i*i;j<n;j+=i){
-                    prime[j] = 0;
-
+                    prime[j]=0;
                 }
             }
         }
-        int cnt=0;
+  
+  int cnt=0;
 
-        for(int i=2;i<prime.length;i++){
-            if(prime[i] == 1){
-                cnt ++;
-            }
-
-        }
-        return cnt;
-        
+  for(int i=2;i<prime.length;i++){
+    if(prime[i] == 1){
+        cnt ++;
+    }
+  }
+  return cnt;
+      
     }
 }
