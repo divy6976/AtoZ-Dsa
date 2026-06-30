bcf9de3834e0691df8966e600335f610d9658d8a
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-30 14:18:40 +0530

    Time: 44 ms (9.42%) | Memory: 103.3 MB (5.01%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,45 +1,50 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+
         if(nums.length == 0){
             return 0;
         }
-   
-              HashSet<Integer> st=new HashSet<>();
 
-        for(int i=0;i<nums.length;i++){
-           st.add(nums[i]);
+        if(nums.length ==1 ){
+            return 1;
         }
 
-        int[] check=new int[st.size()];
-      int j=0;
-      for(int s:st){
-        check[j++]=s;
 
-      }
-      Arrays.sort(check);
+   int maxi=0;
+
+   HashSet<Integer> st=new HashSet<>();
+    ArrayList<Integer> ans=new ArrayList<>();
+  
+
+   for(int i=0;i<nums.length;i++){
+    st.add(nums[i]);
+   }
+  for(int s:st){
+    ans.add(s);
+  }
+ Collections.sort(ans);
+  
 
 
-        int maxi=1;
+int cnt=1;
+   for(int i=1;i<ans.size();i++){
+    int compare=ans.get(i)-1;
 
-        int cnt =1;
+    if(ans.get(i-1) ==  compare){
+        cnt ++;
+    }else {
+        cnt=1;
+    }
+    maxi=Math.max(maxi,cnt);
 
-        for(int i=1;i<check.length;i++){
-            int prev=check[i-1]+1;
 
+   }
 
+   maxi=Math.max(maxi,cnt);
 
-            if(check[i] != prev){
-                maxi=Math.max(cnt,maxi);
-                cnt=1;
+   return maxi;
 
-            }else {
-            cnt ++;
-        }
-        }
-        maxi=Math.max(maxi,cnt);
-        
-  return maxi;
-
+       
         
     }
 }
