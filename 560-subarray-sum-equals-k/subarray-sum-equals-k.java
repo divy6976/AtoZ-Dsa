85b26249cc352e9eb69c7378a1e1ccf3018eca8c
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-30 17:15:23 +0530

    Time: 18 ms (99.83%) | Memory: 49.5 MB (5.55%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,23 +1,30 @@
 class Solution {
-
-    
     public int subarraySum(int[] nums, int k) {
 
 
-   int cnt =0;
 
-   for(int i=0;i<nums.length;i++){
-    int sum =0;
-    for(int j=i;j<nums.length;j++){
-        sum += nums[j];
-        if(sum == k){
-            cnt ++;
+        int[] prefix=new int[nums.length];
+
+        prefix[0]=nums[0];
+
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
         }
-      
-    }
-    
-   }
-return cnt;
+
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+
+        mpp.put(0,1);
+        int sum=0;
+
+        for(int i=0;i<nums.length;i++){
+            int check=prefix[i]-k;
+
+            if(mpp.containsKey(check)){
+                sum += mpp.get(check);
+            }
+            mpp.put(prefix[i],mpp.getOrDefault(prefix[i],0)+1);
+        }
+        return sum ;
         
     }
 }
