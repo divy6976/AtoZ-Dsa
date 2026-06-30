d2fe764d5ef1b5fcb375d03cfb03dbea413eb655
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-06-30 17:21:22 +0530

    Time: 16 ms (46.85%) | Memory: 54.3 MB (7.34%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,27 +1,26 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        return lessthan(nums,goal)-lessthan(nums,goal-1);
-    }
+        int prefix[]=new int[nums.length];
+        prefix[0]=nums[0];
 
-public int lessthan(int[] nums,int goal ){
-    if(goal<0){
-        return 0;
-    }
-    int l=0;
-    int r=0;
-    int sum=0;
-    int cnt=0;
-    while(r<nums.length){
-        sum +=nums[r];
-        while(sum >goal){
-            sum=sum-nums[l];
-            l=l+1;
+        for(int i=1;i<nums.length;i++){
+            prefix[i]=prefix[i-1]+nums[i];
             
         }
-        cnt=cnt + r-l+1;
-        r=r+1;
-   
-}
- return cnt;
-}
+
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+
+        mpp.put(0,1);
+        int sum=0;
+
+        for(int i=0;i<nums.length;i++){
+            int check=prefix[i]-goal;
+            if(mpp.containsKey(check)){
+                sum += mpp.get(check);
+            }
+            mpp.put(prefix[i],mpp.getOrDefault(prefix[i],0)+1);
+        }
+
+        return sum;
+    }
 }
