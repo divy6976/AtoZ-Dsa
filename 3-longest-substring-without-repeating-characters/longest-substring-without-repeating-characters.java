9038b27440ecfdd789632fd9d4b01b3ae5594e5a
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-01 09:58:59 +0530

    Time: 10 ms (12.98%) | Memory: 46.8 MB (11.11%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -5,32 +5,34 @@ class Solution {
             return 0;
         }
 
-
         int l=0;
         int r=0;
-        int length=1;
+        int maxi=1;
 
-        HashMap<Character,Integer> mpp=new HashMap<>();
+        HashMap<Character,Integer>mpp=new HashMap<>();
 
-        while(r< s.length()){
-            char ch=s.charAt(r);
 
-            while(mpp.containsKey(ch)){
+        while(r<s.length()){
 
-                 mpp.put(s.charAt(l) , mpp.getOrDefault(s.charAt(l),0)-1);
-
-                 if(mpp.get(s.charAt(l)) == 0){
-      mpp.remove(s.charAt(l));
-                 }
-                 l++;
-
-                
+            while(mpp.containsKey(s.charAt(r))){
+                mpp.put(s.charAt(l),mpp.getOrDefault(s.charAt(l),0)-1);
+                if(mpp.get(s.charAt(l)) == 0){
+                    mpp.remove(s.charAt(l));
+                }
+                l++;
             }
-            mpp.put(ch,mpp.getOrDefault(ch,0)+1);
-            length=Math.max(length,r-l+1);
-  r++;
 
+
+
+
+
+            mpp.put(s.charAt(r),mpp.getOrDefault(s.charAt(r),0)+1);
+            maxi=Math.max(maxi,r-l+1);
+            r++;
         }
-      return length;  
+
+        return maxi;
+
+        
     }
 }
