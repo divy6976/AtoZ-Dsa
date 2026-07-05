6ad7d901dbe7f6b242643cf4c03ab4b344ab8067
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-05 16:18:41 +0530

    Time: 1 ms (94.04%) | Memory: 43.4 MB (51.58%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,36 +1,32 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-
+    public List<List<Integer>> generate(int numRows) { 
 
         List<List<Integer>> ans=new ArrayList<>();
 
         for(int i=0;i<numRows;i++){
-            List<Integer> sublist=new ArrayList<>();
-
+            ArrayList<Integer> sublist=new ArrayList<>();
             if(i==0){
-
-                sublist.add(1);           
+                sublist.add(1);
 
             }else if(i==1){
                 sublist.add(1);
                 sublist.add(1);
-
             }else {
-                List<Integer> compare=ans.get(ans.size()-1); 
-               for(int k=0;k<compare.size();k++){
-                if(k==0){
-                    sublist.add(1);
-                }else {
-                    sublist.add(compare.get(k)+compare.get(k-1));
+                sublist.add(1);
+
+                List<Integer> prev=ans.get(ans.size()-1);
+                for(int m=0;m<prev.size()-1;m++){
+                    sublist.add(prev.get(m) + prev.get(m+1));
                 }
-               }
-               sublist.add(1);
+                sublist.add(1);
             }
 
 
-ans.add(new ArrayList<>(sublist));
+            ans.add(new ArrayList<>(sublist));
         }
         return ans;
+
+
         
     }
 }
