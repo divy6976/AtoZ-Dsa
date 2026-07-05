8b1930f279fadde380d9a03c73ddeb3fea5585d2
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-05 09:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (68.76%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -2,60 +2,61 @@ class Solution {
     public void nextPermutation(int[] nums) {
 
 
-        // step 1   
-    // find pivot  a[i] < a[i+1];
 
-   int j=nums.length-2;
-   int pivot=-1;
+        int pivot=-1;
 
-   while(j>=0){
-    if(nums[j] <nums[j+1]){
+       for(int i=nums.length-1;i>0;i--){
+         if(nums[i-1] < nums[i]){
+            pivot=i-1;
+            break;
+         }
+       }
 
-        pivot=j;
 
-        break;
-    }
-    j--;
+       if(pivot == -1 ){
 
-   }
+        int i=0;
+        int j=nums.length-1;
 
-   if(pivot == -1){
-    int i=0;
-    int k=nums.length-1;
-   while(i<k){
-    int temp=nums[i];
-    nums[i]=nums[k];
-    nums[k]=temp;
-    i++;k--;
-   }
-   return;
-   }
-// step2 find rightmost bigger
-int maxiind=-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
 
-   for(int i=nums.length-1;i>=pivot;i--){
+        return ;
+    
+       }
 
+        int small=-1;
+       int mini=Integer.MAX_VALUE;
+
+       for(int i=nums.length-1;i>pivot;i--){
     if(nums[i] > nums[pivot]){
-        maxiind=i;
+        small = i;
         break;
     }
+       }
 
-   }
+       int temp=nums[pivot];
+       nums[pivot]=nums[small];
+       nums[small]=temp;
 
-   int tempo=nums[pivot];
-   nums[pivot]=nums[maxiind];
-   nums[maxiind]=tempo;
+       int m= pivot+1;
+       int n=nums.length-1;
 
-   int left = pivot + 1;
-int right = nums.length - 1;
+       while(m<n){
+        int temps = nums[m];
+        nums[m]=nums[n];
+        nums[n]=temps;
+        m++;
+        n--;
+       }
 
-while (left < right) {
-    int temp = nums[left];
-    nums[left] = nums[right];
-    nums[right] = temp;
-    left++;
-    right--;
-}
+       return;
+
 
    
         
