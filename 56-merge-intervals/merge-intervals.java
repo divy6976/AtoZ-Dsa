087420167c4c78b7bfdda845db81edf53efc908d
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-05 20:47:35 +0530

    Time: 8 ms (91.49%) | Memory: 48.8 MB (91.81%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,20 +1,40 @@
 class Solution {
-
     public int[][] merge(int[][] intervals) {
-        List<int[]> ans=new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-     
-int overlap[]=intervals[0];
-for(int i=0;i<intervals.length;i++){
-    if(intervals[i][0]<=overlap[1]){
-        overlap[1]=Math.max(intervals[i][1],overlap[1]);
-    }else{
-        ans.add(overlap);
-      overlap=intervals[i];
-    }
-}
-ans.add(overlap);
 
-return ans.toArray(new int[ans.size()][]);
+
+       Arrays.sort(intervals, (a, b) -> {
+    return a[0] - b[0];
+});
+
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+
+        List<int[]> list=new ArrayList<>();
+
+        for(int i=1;i<intervals.length;i++){
+            int a=intervals[i][0];
+            int b=intervals[i][1];
+
+            if( a <= end && start <= b ){
+
+                end=Math.max(end,b);       
+         }else if(a<=end && start > b){
+            start=Math.min(start,a);
+            end=Math.max(end,b);
+         }else if(a > end){
+            list.add(new int[]{start,end});
+            start=a;
+            end=b;
+
+         }
+        }
+        list.add(new int[]{start,end});
+        int[][] ans=new int[list.size()][2];
+        for(int i=0;i<ans.length;i++){
+            ans[i][0]=list.get(i)[0];
+            ans[i][1]=list.get(i)[1];
+        }
+        return ans;
     }
 }
+
