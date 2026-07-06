5cd50c57f537a400dba7478676da6f82f5dab2c4
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-06 11:43:55 +0530

    Time: 1 ms (93.06%) | Memory: 47.1 MB (72.61%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,44 +1,37 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
-        if(nums.length ==1){
-            return nums[0];
+        int lefty=1;
+      int  righty=1;
+        int left=Integer.MIN_VALUE;
+        int right=Integer.MIN_VALUE;
+
+
+        int i=0;
+        int j=nums.length-1;
+
+        while(i<nums.length && j >=0){
+            lefty=lefty * nums[i];
+            
+            left=Math.max(left,lefty);
+
+            if(lefty == 0){
+                lefty=1;
+            }
+
+            righty =righty * nums[j];
+            right = Math.max(right,righty);
+            if(righty == 0){
+                righty =1;
+            }
+            i++;
+            j--;
+
         }
 
-        int left =1;
-        int maxileft=Integer.MIN_VALUE;
+        return Math.max(left,right);
 
-        int right =1;
-        int maxiright=Integer.MIN_VALUE;
-
-      int i=0;
-      int j=nums.length-1;
-
-      while(i< nums.length && j>=0){
-        left =left * nums[i];
-
-        maxileft=Math.max(left,maxileft);
-        if(left == 0){
-            left =1;
-        }
-
-        right =right * nums[j];
-        maxiright=Math.max(right,maxiright);
-
-        if(right == 0){
-            right =1;
-        }
-
-        i++;
-        j--;
         
-        
-
-      }
-      return Math.max(maxileft,maxiright);
-
-
-
         
     }
 }
