c00f6ad31bef0038e69679a0bb2147b03f498c40
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-12 09:56:13 +0530

    Time: 1 ms (39.46%) | Memory: 46.9 MB (44.10%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,62 +1,42 @@
 class Solution {
-    class Pair {
+    class Pair{
         int row;
         int col;
-        
-
-        Pair(int row, int col) {
-            this.row = row;
-            this.col = col;
-            
+        Pair(int row,int col){
+            this.row=row;
+            this.col=col;
         }
+
     }
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n=image.length;
-        int m=image[0].length;
 
-     Queue<Pair> q = new LinkedList<>();
-       
-        int[][] visited = new int[n][m];
+        Queue<Pair> q=new LinkedList<>();
+boolean[][] visited=new boolean[image.length][image[0].length];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                visited[i][j]=image[i][j];
+int original=image[sr][sc];
+
+        q.offer(new Pair(sr,sc));
+        image[sr][sc]=color;
+        visited[sr][sc]=true;
+ int dr[]={-1,0,1,0};
+ int dc[]={0,1,0,-1};
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            int  row=p.row;
+            int col=p.col;
+
+
+            for(int k=0;k<4;k++){
+                int nr=row+dr[k];
+                int nc=col+dc[k];
+                if(nr>=0 && nr< image.length && nc>=0 && nc<image[0].length && image[nr][nc]==original    && !visited[nr][nc]){
+                    q.offer(new Pair(nr,nc));
+                    visited[nr][nc]=true;
+                    image[nr][nc]=color;
+                }
             }
+
         }
-
-     int startColor = image[sr][sc];
-if (startColor == color) return visited; 
-
-visited[sr][sc] = color;
-q.offer(new Pair(sr, sc));
-
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            int r = p.row;
-            int c = p.col;
-            
-
-            if (r - 1 >= 0 && image[r - 1][c] ==image[sr][sc]  && visited[r - 1][c] != color) {
-                visited[r - 1][c] = color;
-                q.offer(new Pair(r - 1, c));
-            }
-
-            if (r + 1 < n && image[r + 1][c] == image[sr][sc] && visited[r + 1][c] != color) {
-                visited[r + 1][c] = color;
-                q.offer(new Pair(r + 1, c));
-            }
-
-            if (c - 1 >= 0 && image[r][c - 1] == image[sr][sc] && visited[r][c - 1] != color) {
-                visited[r][c - 1] = color;
-                q.offer(new Pair(r, c - 1));
-            }
-
-            if (c + 1 < m && image[r][c + 1] == image[sr][sc] && visited[r][c + 1] != color) {
-                visited[r][c + 1] = color;
-                q.offer(new Pair(r, c + 1));
-            }
-        }
-return visited;
+        return image;
     }
 }
