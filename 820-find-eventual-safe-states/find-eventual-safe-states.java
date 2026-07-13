8f90b1af3c66ae72276a6ceba3f855b05db4b129
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-13 09:37:11 +0530

    Time: 25 ms (43.03%) | Memory: 60.4 MB (85.32%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,47 +1,51 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
+
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
 
         for(int i=0;i<graph.length;i++){
             adj.add(new ArrayList<>());
         }
+        int[] indegree=new int[graph.length];
+        int k=0;
 
-       Queue<Integer> q=new LinkedList<>();
+        for(int [] arr:graph){
+          
 
-       int[] outdegree=new int[graph.length];
-
-        for(int i=0;i<graph.length;i++){
-            if(graph[i].length == 0){
-               q.offer(i);
+            for(int m=0;m<arr.length;m++){
+           adj.get(arr[m]).add(k);   // reverse edge
+indegree[k]++;  
             }
-            for(int j=0;j<graph[i].length;j++){
-                adj.get(graph[i][j]).add(i);
+
+           
+
+            k++;
+           
+          
+        }
+ List<Integer> ans=new ArrayList<>();
+
+        Queue<Integer> q=new LinkedList<>();
+
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i] == 0){
+                q.offer(i);
             }
-            outdegree[i]=graph[i].length;
         }
 
-        ArrayList<Integer> ans=new ArrayList<>();
-
-        
         while(!q.isEmpty()){
             int num=q.poll();
             ans.add(num);
-
             for(int neighbour:adj.get(num)){
-                outdegree[neighbour]--;
-                if(outdegree[neighbour] == 0){
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0){
                     q.offer(neighbour);
-                }
-
-
+                } 
             }
         }
 
-Collections.sort(ans);
-return ans;
-    
-
-
+           Collections.sort(ans);
+           return ans;
 
         
     }
