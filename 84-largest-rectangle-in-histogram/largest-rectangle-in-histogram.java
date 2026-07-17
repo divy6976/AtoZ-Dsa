297d497eb492c24fa5364eace99e6aa23e7cdf34
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-07-17 09:59:39 +0530

    Time: 100 ms (33.84%) | Memory: 77.3 MB (76.93%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,67 +1,63 @@
 class Solution {
+    void nsee(int[] nse,int[] heights){
 
-    void psee(int[] heights,int[] pse){
-        Stack<Integer>st=new Stack<>();
+        Stack<Integer> st=new Stack<>();
 
-        for(int i=0;i<heights.length;i++){
+        for(int i=heights.length-1;i>=0;i--){
+
             while(!st.isEmpty() && heights[i] <= heights[st.peek()]){
                 st.pop();
-            }
-              if(st.isEmpty()){
-                pse[i]=-1;
-               
-            }else {
-                pse[i]=st.peek();
-                
 
             }
-            st.push(i);
-    }
-    }
-    void nsee(int[] heights,int[] nse){
-                Stack<Integer>st=new Stack<>();
-  
-        for(int i=heights.length-1;i>=0;i--){
-            while(!st.isEmpty() && heights[i] <=heights[st.peek()]){
-                st.pop();
-            }
+
             if(st.isEmpty()){
-                nse[i]=nse.length;
-               
+                nse[i]=heights.length;
             }else {
                 nse[i]=st.peek();
-                
-
             }
             st.push(i);
         }
+
     }
+
+    void psee(int[] pse,int[] heights){
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=0;i<heights.length;i++){
+
+            while(!st.isEmpty() && heights[i] <= heights[st.peek()]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                pse[i]=-1;
+            }else {
+                pse[i]=st.peek();
+            }
+  st.push(i);
+
+        }
+
+    }
+
+
     public int largestRectangleArea(int[] heights) {
 
-        int[] nse=new int[heights.length];
+        int nse[]=new int[heights.length];
         int[] pse=new int[heights.length];
 
-
-        nsee(heights,nse);
-        psee(heights,pse);
-
-
+        nsee(nse,heights);
+        psee(pse,heights);
         int maxi=0;
 
         for(int i=0;i<heights.length;i++){
 
-         int breadth=0;
-         breadth +=i-pse[i];
-         breadth += nse[i]-(i+1);
-
-         int area= breadth* heights[i];
-         maxi=Math.max(maxi,area);
+            int width=nse[i]-pse[i]-1;
+            int area=heights[i] * width;
 
 
+            maxi=Math.max(maxi,area);
 
-            
         }
         return maxi;
-        
     }
 }
