477edab965f16aa50d614bfc3263046e808a67bd
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-08-08 21:06:51 +0530

    Time: 34 ms (60.89%) | Memory: 59.5 MB (24.11%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,48 +1,49 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-Arrays.sort(nums);
-        List<List<Integer>> ans=new ArrayList<>();
+    List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        //x+y+z=0;
+        //y+z=-x;
 
-        for(int i=0;i<nums.length;i++){
-            if(i>0 && nums[i]== nums[i-1]){
-                continue;
-            }
+        for(int i=0;i<nums.length-2;i++){
+   
+   if(i > 0 && nums[i] == nums[i-1]){
+    continue;
+}
+            int check=nums[i];
+            check=-nums[i];
+
             int j=i+1;
-            int k=nums.length-1;
-
-            int target=-(nums[i]);
+            int k= nums.length-1;
 
             while(j<k){
-   if((nums[j] + nums[k]) == target){
-List<Integer> sublist=new ArrayList<>();
-sublist.add(nums[i]);
-sublist.add(nums[j]);
-sublist.add(nums[k]);
+                if(nums[j] + nums[k] == check){
+                    ArrayList<Integer> sublist=new ArrayList<>();
+      sublist.add(nums[i]);
+      sublist.add(nums[j]);
+      sublist.add(nums[k]);
+      ans.add(new ArrayList<>(sublist));
+      j++;
+      k--;
 
-ans.add(new ArrayList<>(sublist));
-j++;
-k--;
-
-
-// Skip duplicate j
-while (j < k && nums[j] == nums[j - 1]) {
-    j++;
-}
-
-// Skip duplicate k
-while (j < k && nums[k] == nums[k + 1]) {
-    k--;
-}
+      while(j<k && nums[j] == nums[j-1]){
+        j++;
+      }
+       while(j<k && nums[k] == nums[k+1]){
+        k--;
+      }
 
 
-   }   else if(nums[j] + nums[k] > target){
-                   k--;
+                }else if(nums[j] + nums[k] > check){
+                    k--;
+
                 }else {
-                   j++;
+                    j++;
                 }
-
             }
+
+
         }
         return ans;
         
