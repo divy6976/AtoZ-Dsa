fec870b42331da2e10f2441ddb1c96c79810a8f1
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-08-15 16:32:43 +0530

    Time: 33 ms (72.70%) | Memory: 59 MB (74.89%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,49 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-    List<List<Integer>> ans=new ArrayList<>();
-        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
         //x+y+z=0;
-        //y+z=-x;
+// y+z=-x
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
 
-        for(int i=0;i<nums.length-2;i++){
-   
-   if(i > 0 && nums[i] == nums[i-1]){
-    continue;
-}
-            int check=nums[i];
-            check=-nums[i];
-
+            int target=-(nums[i]);
             int j=i+1;
-            int k= nums.length-1;
-
+            int k=nums.length-1;
             while(j<k){
-                if(nums[j] + nums[k] == check){
+                int sum = nums[j]+nums[k];
+                if(sum == target){
                     ArrayList<Integer> sublist=new ArrayList<>();
-      sublist.add(nums[i]);
-      sublist.add(nums[j]);
-      sublist.add(nums[k]);
-      ans.add(new ArrayList<>(sublist));
-      j++;
-      k--;
-
-      while(j<k && nums[j] == nums[j-1]){
-        j++;
-      }
-       while(j<k && nums[k] == nums[k+1]){
-        k--;
-      }
-
-
-                }else if(nums[j] + nums[k] > check){
+                    sublist.add(nums[i]);
+                    sublist.add(nums[j]);
+                    sublist.add(nums[k]);
+                    ans.add(new ArrayList<>(sublist));
+                    j++;
                     k--;
-
+                    while(j<k && nums[j]== nums[j-1]){
+                        j++;
+                    }
+                    while(j<k && nums[k]== nums[k+1]){
+                        k--;
+                    }
+                }else if(sum > target){
+                    k--;
                 }else {
                     j++;
                 }
             }
-
-
         }
         return ans;
         
