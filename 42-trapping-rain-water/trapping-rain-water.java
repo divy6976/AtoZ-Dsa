a2a082b031e7c34d772becf99fc5213f7572fbe1
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-08-15 16:23:22 +0530

    Time: 1 ms (58.48%) | Memory: 48.6 MB (27.86%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,57 +1,42 @@
 class Solution {
 
-
-    void leftymax(int[] height,int[] lefty){
-        int maxi=Integer.MIN_VALUE;
-        for(int i=0;i<height.length;i++){
-            if(height[i] > maxi){
-                maxi=height[i];
-lefty[i]=-1;
-            }else{
-                lefty[i]=maxi;
-            }
-
+void righty(int[] height,int[] rightmax){
+    int maxi=Integer.MIN_VALUE;
+    for(int i=height.length-1;i>=0;i--){
+        if(height[i] > maxi){
+            rightmax[i]=-1;
+            maxi=height[i];
+        }else {
+            rightmax[i]=maxi;
         }
-
     }
-
-
-    void rightmax(int[] height,int[] righty){
-
-
-        int maxi=Integer.MIN_VALUE;
-
-        for(int i=height.length-1;i>=0;i--){
-
-            if(height[i] > maxi){
-                maxi=height[i];
-       righty[i]=-1;
-            }else {
-                righty[i]=maxi;
-            }
-
+    
+}
+void lefty(int[] height,int[] leftmax){
+    int maxi=Integer.MIN_VALUE;
+    for(int i=0;i<height.length;i++){
+        if(height[i]>maxi){
+            leftmax[i]=-1;
+            maxi=height[i];
+        }else {
+            leftmax[i]=maxi;
         }
-
-
-
-
     }
+}
+
     public int trap(int[] height) {
+int[] rightmax=new int[height.length];
+int[] leftmax=new int[height.length];
 
+ righty(height,rightmax);
+ lefty(height,leftmax);
+int sum =0;
+ for(int i=0;i<height.length;i++){
+if(rightmax[i]!=-1 && leftmax[i]!=-1){
+    sum += Math.min(rightmax[i],leftmax[i]) - height[i];
+}
 
-    int[] righty=new int[height.length];
-    int[] lefty=new int[height.length];
-        rightmax(height,righty);
-        leftymax(height,lefty);
-        int sum=0;
-        for(int i=0;i<height.length;i++){
-
-         if(righty[i] !=-1 && lefty[i]!=-1){
-            sum += Math.min(lefty[i],righty[i]) - height[i];
-         }
-
-        }
-        return sum;
-
+ }
+return sum;
     }
 }
