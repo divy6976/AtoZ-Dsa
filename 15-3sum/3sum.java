a57edec3fa11a4d64a69cdd4a60a89a5bfd2952a
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-08-24 11:06:21 +0530

    Time: 37 ms (22.95%) | Memory: 59 MB (74.59%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,41 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        
         List<List<Integer>> ans=new ArrayList<>();
-        //x+y+z=0;
-// y+z=-x
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
+Arrays.sort(nums);
+  // x+y+z =0;
+        for(int i=0;i<nums.length-2;i++){
+
+    if(i>0 && nums[i] == nums[i-1]){
+        
+        continue;
+    }
+       int target=-(nums[i]);
+       int j=i+1;
+       int k=nums.length-1;
+       while(j<k){
+        int sum = nums[j]+nums[k];
+
+        if(sum == target){
+            List<Integer> sublist=new ArrayList<>();
+            sublist.add(nums[i]);
+            sublist.add(nums[j]);
+            sublist.add(nums[k]);
+            ans.add(new ArrayList<>(sublist));
+            j++;
+            k--;
+            while(j<k && nums[j] == nums[j-1]){
+                j++;
+
+            }  
+            while(j<k && nums[k] == nums[k+1]){
+                k--;
             }
 
-            int target=-(nums[i]);
-            int j=i+1;
-            int k=nums.length-1;
-            while(j<k){
-                int sum = nums[j]+nums[k];
-                if(sum == target){
-                    ArrayList<Integer> sublist=new ArrayList<>();
-                    sublist.add(nums[i]);
-                    sublist.add(nums[j]);
-                    sublist.add(nums[k]);
-                    ans.add(new ArrayList<>(sublist));
-                    j++;
-                    k--;
-                    while(j<k && nums[j]== nums[j-1]){
-                        j++;
-                    }
-                    while(j<k && nums[k]== nums[k+1]){
-                        k--;
-                    }
-                }else if(sum > target){
-                    k--;
-                }else {
-                    j++;
-                }
-            }
+        }else if(sum > target){
+            k--;
+        }else {
+            j++;
         }
-        return ans;
         
+       }
+     
+        }
+          return ans;
+            
+
     }
 }
