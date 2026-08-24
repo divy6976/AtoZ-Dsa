d03cccfd0dd909ceee9db095c9c6619fe19b8295
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-08-24 19:28:15 +0530

    Time: 1 ms (58.42%) | Memory: 48.7 MB (15.37%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,42 +1,45 @@
 class Solution {
 
-void righty(int[] height,int[] rightmax){
-    int maxi=Integer.MIN_VALUE;
-    for(int i=height.length-1;i>=0;i--){
-        if(height[i] > maxi){
-            rightmax[i]=-1;
-            maxi=height[i];
-        }else {
-            rightmax[i]=maxi;
-        }
+    void lefty(int[] height,int[] leftmax){
+  int maxi=-1;
+  for(int i=0;i<height.length;i++){
+    if(height[i] > maxi){
+        maxi=height[i];
+        leftmax[i]=-1;
+    }else {
+        leftmax[i]=maxi;
     }
-    
-}
-void lefty(int[] height,int[] leftmax){
-    int maxi=Integer.MIN_VALUE;
-    for(int i=0;i<height.length;i++){
-        if(height[i]>maxi){
-            leftmax[i]=-1;
-            maxi=height[i];
-        }else {
-            leftmax[i]=maxi;
-        }
+  }
     }
+    void righty(int[] height,int[] rightmax){
+  
+  int maxi=-1;
+        for(int i=height.length-1;i>=0;i--){
+if(height[i] > maxi){
+    maxi=height[i];
+    rightmax[i]=-1;
+}else {
+    rightmax[i]=maxi;
 }
+        }
 
+    }
     public int trap(int[] height) {
-int[] rightmax=new int[height.length];
-int[] leftmax=new int[height.length];
 
- righty(height,rightmax);
- lefty(height,leftmax);
-int sum =0;
- for(int i=0;i<height.length;i++){
-if(rightmax[i]!=-1 && leftmax[i]!=-1){
-    sum += Math.min(rightmax[i],leftmax[i]) - height[i];
+
+        int[] leftmax=new int[height.length];
+        int[] rightmax=new int[height.length];
+
+        lefty(height,leftmax);
+        righty(height,rightmax);
+
+
+int sum=0;
+for(int i=0;i<height.length;i++){
+if(leftmax[i] !=-1 && rightmax[i]!=-1){
+    sum += Math.min(leftmax[i],rightmax[i])-height[i];
 }
-
- }
-return sum;
+}
+        return sum;
     }
 }
