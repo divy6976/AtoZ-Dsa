642c70e180bca31cea74b88d1859cb29ac5bdf42
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-09-02 11:10:02 +0530

    Time: 4 ms (28.04%) | Memory: 46.4 MB (69.23%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -4,56 +4,47 @@ class Solution {
 
         for(int i=0;i<board.length;i++){
             HashSet<Character> st=new HashSet<>();
-
             for(int j=0;j<board[0].length;j++){
-
-                if(st.contains(board[i][j])){
+                 if(st.contains(board[i][j])){
                     return false;
-                }
-                if(board[i][j] == '.'){
-                    continue;
-                }
-                st.add(board[i][j]);
+                  }
+                  if(board[i][j]!='.'){
+                  st.add(board[i][j]);
+                  }
             }
         }
 
-        for(int i=0;i<board[0].length;i++){
+        for(int j=0;j<board[0].length;j++){
             HashSet<Character> st=new HashSet<>();
-            for(int j=0;j<board.length;j++){
-             if(board[j][i] == '.') continue;
-
-if(st.contains(board[j][i])){
-    return false;
-}
-
-st.add(board[j][i]);
+            for(int i=0;i<board.length;i++){
+                if(st.contains(board[i][j])){
+                    return false;
+                } 
+                if(board[i][j]!='.'){
+                  st.add(board[i][j]);
+                  }
             }
         }
 
         for(int i=0;i<board.length;i+=3){
             for(int j=0;j<board[0].length;j+=3){
-
-                HashSet<Character> st=new HashSet<>();
-
-                for(int m=i;m<=i+2;m++){
-                    for(int n=j;n<=j+2;n++){
+HashSet<Character> st=new HashSet<>();
+                for(int m=i;m<i+3;m++){
+                    
+                    for(int n=j;n<j+3;n++){
                         if(st.contains(board[m][n])){
                             return false;
                         }
-                        if(board[m][n] == '.'){
-                            continue;
-                        }
-                        st.add(board[m][n]);
+                if(board[m][n]!='.'){
+                  st.add(board[m][n]);
+                  }
                     }
                 }
-
             }
         }
- return true;
+        return true;
+
+    
         
-
-
-
-                
     }
 }
