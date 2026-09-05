e810efa75d1e27e37ca91e50e907435eb43b6f56
Divy Prakash Pandey <divyprakashpandey6@gmail.com>
2026-09-05 09:32:12 +0530

    Time: 963 ms (5.03%) | Memory: 47.2 MB (5.30%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,45 +1,40 @@
 class Solution {
-
     public boolean checkInclusion(String s1, String s2) {
-       
-       char[] ch=s1.toCharArray();
-       Arrays.sort(ch);
-    StringBuilder sb=new StringBuilder();
-    for(int i=0;i<ch.length;i++){
-        sb.append(ch[i]);
-    }
 
-    String s=sb.toString();
-    int len=s.length();
+              StringBuilder sb=new StringBuilder();
 
-    int l=0;
-    int r=0;
+              char[] ch=s1.toCharArray();
+              Arrays.sort(ch);
 
-    while(r<s2.length()){
+              
 
-        while((r-l+1) > len){
-            l++;
-        }
+              for(int i=0;i<ch.length;i++){
+                sb.append(ch[i]);
+              }
+              String s=sb.toString();
 
-        if((r-l+1) == len){
-            String part=s2.substring(l,r+1);
-            char[] dh=part.toCharArray();
-            Arrays.sort(dh);
-            StringBuilder sbs=new StringBuilder();
-            for(int i=0;i<dh.length;i++){
-                sbs.append(dh[i]);
+            int l=0;
+            int r=0;
+
+            while(r<s2.length()){
+                
+                if((r-l+1) == s1.length()){
+                    char[] dh=s2.substring(l,r+1).toCharArray();
+                    Arrays.sort(dh);
+                    StringBuilder sbs=new StringBuilder();
+                    for(int i=0;i<dh.length;i++){
+                        sbs.append(dh[i]);
+                    }
+       String d= sbs.toString();
+       if(s.equals(d)){
+        return true;
+       }
+
+     l++;
+                }
+                r++;
             }
-            String compare=sbs.toString();
-        
-           if(compare.equals(s)){
-            return true;
-           }
-            
-        }
-        r++;
-    }
- return false;
 
-
+        return false;
     }
 }
